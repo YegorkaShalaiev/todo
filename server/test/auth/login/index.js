4d58e0ff82cf5acac60e60852acaba2d64d58eae
Yegor <yegorshalaev@gmail.com>
2022-05-26 15:02:04 +0300

    [#9] Add test frameworks, implement example tests

---
+++ server/test/auth/login/index.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+describe('Auth: Login', function () {
+    it('should do something', function () {
+        expect(1).to.be.a('number');
+    });
+
+    it('should do something wrong', function () {
+        expect(null).to.be.a('string');
+    });
+});
